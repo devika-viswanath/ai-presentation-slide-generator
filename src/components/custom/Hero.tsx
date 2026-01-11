48d0f580863d0ba71bda7caa6920e279c295767d
Devika B <devika.workhub@gmail.com>
2026-01-12 00:06:49 +0530

    Verified overall appearance and alignment

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -1,17 +1,37 @@
 import { Play, Video } from 'lucide-react'
 import React from 'react'
 import { Button } from '../ui/button'
+import { HeroVideoDialog } from '@/components/ui/hero-video-dialog'
+
+
 
 
 function Hero() {
   return (
     <div className='flex flex-col items-center justify-center mt-28 space-y-4'>
-      <h2 className='font-bold text-5xl'>From Idea to Presentation in one Click ⚡</h2>
+      <h2 className='font-bold text-5xl'>From Idea to <span className='text-primary'>Presentation</span> in one Click ⚡</h2>
       <p className='text-xl text-gray-500 max-w-2xl text-center'>Generate sleek, editable PPT decks in minutes. AI handle slide design, formatting and visual content so you can focus on your message, impress your audience, and work smarter, not harder.</p>
       <div className='flex gap-5 mt-10'>
         <Button variant={'outline'} size={'lg'}> Watch Video <Play /> </Button>
         <Button size={'lg'}>Get Started</Button>
       </div>
+       <div className="relative max-w-3xl mt-14">
+        <h2 className='text-center my-4'>Watch how to Create AI PPT</h2>
+      <HeroVideoDialog
+        className="block dark:hidden"
+        animationStyle="from-center"
+        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+        thumbnailAlt="Hero Video"
+      />
+      <HeroVideoDialog
+        className="hidden dark:block"
+        animationStyle="from-center"
+        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        thumbnailAlt="Hero Video"
+      />
+    </div>
     </div>
   )
 }
