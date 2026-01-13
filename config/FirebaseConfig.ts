b5e98b7899965d02126516dae12f968766e0fa90
Devika B <devika.workhub@gmail.com>
2026-01-13 23:12:08 +0530

    Configure Firebase Firestore and store authenticated user details

---
+++ config/FirebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "ai-ppt-generator-b75ac.firebaseapp.com",
+  projectId: "ai-ppt-generator-b75ac",
+  storageBucket: "ai-ppt-generator-b75ac.firebasestorage.app",
+  messagingSenderId: "336499128590",
+  appId: "1:336499128590:web:c3bf7d4117274cbafc7f5f",
+  measurementId: "G-0YCLVCR4ZL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firebaseDb = getFirestore(app);
--- src/workspace/index.tsx
+++ src/workspace/index.tsx
@@ -1,19 +1,50 @@
 import { useUser } from '@clerk/clerk-react';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { Button } from '../components/ui/button';
+import { firebaseDb } from './../../config/FirebaseConfig';
+import { doc, getDoc, setDoc } from "firebase/firestore"; // Added setDoc import
 
 function Workspace() {
-  const { user } = useUser();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user])
+    
+  // If user is not signed in
+  const CreateNewUser = async () => {
+    if (user) {
+      // If user already exists in DB
+      const docRef = doc(firebaseDb, "users", user?.primaryEmailAddress?.emailAddress);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // Insert new user
+        await setDoc(doc(firebaseDb, "users", 
+          user.primaryEmailAddress?.emailAddress), {
+            fullName: user?.fullName,
+            email: user?.primaryEmailAddress?.emailAddress, 
+            createdAt: new Date(),
+            credits: 2,
+        })
+      }
+    }
+  } 
 
   if (!user) {
-    return <div>Please sign in to access the workspace.
-      <Link to="/">
-        <Button>Sign In</Button>
-      </Link>
-    </div>;
+    return (
+      <div>Please sign in to access the workspace.
+        <Link to="/">
+          <Button>Sign In</Button>
+        </Link>
+      </div>
+    );
   }
+
   return (
     <div>
       Workspace
