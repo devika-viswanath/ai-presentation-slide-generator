eb3de9a9e7a2689a082b6102f56107645936ea3d
Devika B <devika.workhub@gmail.com>
2026-01-14 15:49:55 +0530

    Implement project creation workflow with Firestore and Clerk authentication

--- src/components/custom/PromptBox.tsx
+++ src/components/custom/PromptBox.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   InputGroup,
   InputGroupAddon,
   InputGroupButton,
-  InputGroupInput,
-  InputGroupText,
   InputGroupTextarea,
 } from "@/components/ui/input-group"
-import { ArrowUp, PlusIcon } from 'lucide-react'
+
+import { ArrowUp, Loader2Icon } from 'lucide-react'
+
 import {
   Select,
   SelectContent,
@@ -18,40 +18,82 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
-const PromptBox = () => {
+import { v4 as uuidv4 } from 'uuid'
+import { doc, setDoc } from 'firebase/firestore'
+import { firebaseDb } from './../../../config/FirebaseConfig'
+import { useUser } from '@clerk/clerk-react'   
+
+function PromptBox() {
+  const [userInput, setUserInput] = useState<string>('');
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+
+  const CreateAndSaveProject = async () => {
+    if (!userInput) return;
+
+    const projectId = uuidv4();
+    setLoading(true);
+
+    try {
+      await setDoc(doc(firebaseDb, "projects", projectId), {
+        projectId,
+        userInputPrompt: userInput,
+        createdBy: user?.primaryEmailAddress?.emailAddress,
+        createdAt: Date.now()
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className='w-full flex items-center justify-center mt-28'>
-    <div className='flex flex-col items-center justify-center space-y-4'> 
-      <h2 className='font-bold text-4xl'>Describe your topic, we'll design the <span className='text-primary'>PPT</span> slides!</h2>
-      <p className='text-xl text-gray-500'>Your design will be saved as new project </p>
-      <InputGroup className='w-[600px]'>
-        <InputGroupTextarea placeholder='Enter what kind of slider do you want to create?' className='min-h-36'/>
-        <InputGroupAddon align={'block-end'}>
-          {/* <InputGroupButton>
-            <PlusIcon/>
-          </InputGroupButton> */}
-          <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select No of Slider" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>No. Of Slider</SelectLabel>
-          <SelectItem value="4 to 6">4-6 Sliders</SelectItem>
-          <SelectItem value="6 to 8">6-8 Sliders</SelectItem>
-          <SelectItem value="8 to 12">8-12 Sliders</SelectItem>
-          
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-            <InputGroupButton variant={'default'} className='rounded-full ml-auto' size={'icon-sm'}>
-              <ArrowUp/>
+      <div className='flex flex-col items-center justify-center space-y-4'>
+        
+        <h2 className='font-bold text-4xl'>
+          Describe your topic, we'll design the <span className='text-primary'>PPT</span> slides!
+        </h2>
+
+        <p className='text-xl text-gray-500'>Your design will be saved as a new project</p>
+
+        <InputGroup>
+          <InputGroupTextarea
+            placeholder='Enter what kind of slides you want to create?'
+            className='min-h-36'
+            onChange={(event) => setUserInput(event.target.value)}
+          />
+
+          <InputGroupAddon align={'block-end'}>
+
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select No of Slides" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>No. Of Slides</SelectLabel>
+                  <SelectItem value="4-6">4-6 Slides</SelectItem>
+                  <SelectItem value="6-8">6-8 Slides</SelectItem>
+                  <SelectItem value="8-12">8-12 Slides</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <InputGroupButton
+              variant={'default'}
+              className='rounded-full ml-auto'
+              size={'icon-sm'}
+              onClick={CreateAndSaveProject}
+              disabled={!userInput}
+            >
+              {loading ? <Loader2Icon className='animate-spin' /> : <ArrowUp />}
             </InputGroupButton>
-        </InputGroupAddon>
-      </InputGroup>
-    </div>
+
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
     </div>
   )
 }
 
-export default PromptBox
+export default PromptBox;
