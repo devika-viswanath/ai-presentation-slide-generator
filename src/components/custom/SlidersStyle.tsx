a1a9082073663a6d1bad40b08143bbb8c1b3523d
Devika B <devika.workhub@gmail.com>
2026-03-09 21:44:41 +0530

    progress update

--- src/components/custom/SlidersStyle.tsx
+++ src/components/custom/SlidersStyle.tsx
@@ -132,4 +132,3 @@ function SlidersStyle({ selectStyle, selectedStyle }: Props) {
 }
 
 export default SlidersStyle
-// Note: The design guide text is truncated for brevity. You can expand it as needed.
